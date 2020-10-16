53a39fc8f077f8ad90dc96e517fcfe620e794ea3
김원진 <wjkim9653@khu.ac.kr>
2020-10-16 16:26:53 +0900

    mult9

--- mult8.cpp
+++ mult8.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
-#include <iomanip>
+//#include <iomanip>
 using namespace std;
 
 int main() {
-	for (int i = 1; i < 9; i++) { // need to change 9 -> 10
+	for (int i = 1; i < 10; i++) { // changed from 9 to 10
 		for (int j = 1; j < 10; j++) {
-			cout << setw(4) << i * j;
+			cout << i << " * " << j << " = " << i * j << endl; //changed the output style from simply displaying the results to displaying the entire process of calculations
+			//this change ended up making the <iomanip> library obsolete, so I annotated the inclusion of <iomanip>
 		}
 		cout << endl;
 	}
